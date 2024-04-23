a0de277c3716203ad18f6c32599686c8c4d53c66
Katherinam4 <84234484+Katherinam4@users.noreply.github.com>
2024-04-22 21:28:25 -0700

    Add files via upload

---
+++ Praqtikuli_Davaleba_3/Praqtikuli_Davaleba_3/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praqtikuli_Davaleba_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] masivi_1 = new int[] { 2, 3, 4, 5, 6 };
+            Klasi_1 obj_1 = new Klasi_1(masivi_1);
+            int Kenti_raod = Klasi_1.Static_Method_1(obj_1);
+            label1.Text = Kenti_raod.ToString();
+            label2.Text = " ";
+            for (int str = 0; str < masivi_1.Length; str++)
+            {
+                label2.Text += masivi_1[str].ToString() + " ";
+            }
+
+        }
+    }
+}
---
+++ Praqtikuli_Davaleba_3/Praqtikuli_Davaleba_3/Klasi_1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praqtikuli_Davaleba_3
+{
+    //    შექმენით Klasi_1 კლასი, რომელიც შეიცავს: მთელი ტიპის ერთგანზომილებიან
+    //სტატიკურ მასივს, რომლის ინიციალიზებას კონსტრუქტორი ახდენს; პრივატულ
+    //ჩვეულებრივ მეთოდს, რომელიც ერთგანზომილებიანი სტატიკური მასივის კენტი
+    //ელემენტების ჯამს გასცემს; ღია სტატიკურ მეთოდს, რომელიც ჩვეულებრივ
+    //მეთოდს იძახებს და გასცემს შედეგს.მთავარ პროგრამაში შექმენით ობიექტი
+    //კონსტრუქტორის გამოყენებით და გამოიძახეთ სტატიკური მეთოდი.
+
+    internal class Klasi_1
+    {
+        private static int[] static_masivi_1;
+        public Klasi_1(int[] par_masivi)
+        {
+            static_masivi_1 = par_masivi;
+        }
+        private int Chveulebrivi_Kenti_Raod()
+        {
+            int raod = 0;
+            for (int str = 0; str < static_masivi_1.Length; str++)
+                if (static_masivi_1[str] % 2 == 1 ||
+                    static_masivi_1[str] % 2 == -1) raod += static_masivi_1[str];
+
+            return raod;
+        }
+        public static int Static_Method_1(Klasi_1 par_obj)
+        {
+            return par_obj.Chveulebrivi_Kenti_Raod();
+
+        }
+    }
+}
