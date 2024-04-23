bb7401e983afcadcaf75cf099ba2d455e050c671
Katherinam4 <84234484+Katherinam4@users.noreply.github.com>
2024-04-22 21:27:05 -0700

    Add files via upload

---
+++ Praqtikuli_Davaleba_1/Praqtikuli_Davaleba_1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praqtikuli_Davaleba_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student student = new Student();
+
+            string gvari, saxeli;
+            int asaki;
+            gvari = textBox1.Text;
+            saxeli = textBox2.Text;
+            asaki = int.Parse(textBox3.Text);
+            student.Minicheba(gvari, saxeli, asaki);
+            student.Gamotana(label4);
+
+        }
+    }
+}
---
+++ Praqtikuli_Davaleba_1/Praqtikuli_Davaleba_1/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praqtikuli_Davaleba_1
+{
+    //    შექმენით სტუდენტის კლასი, რომელიც შეიცავს პრივატულ ცვლადებს: გვარი,
+    //სახელი და ასაკი; ღია მეთოდებს: პირველი მეთოდი პრივატულ ცვლადებს
+    //მნიშვნელობებს ანიჭებს; მეორე მეთოდს ეკრანზე გამოაქვს პრივატული
+    //ცვლადების მნიშვნელობები.მთავარ პროგრამაში ნაგულისხმევი
+    //კონსტრუქტორის გამოყენებით შექმენით შესაბამისი ობიექტი და გამოიძახეთ
+    //ორივე მეთოდი.მონაცემების შესატანად გამოიყენეთ TextBox ვიზუალური
+    //ელემენტი, გამოსატანად კი Label ვიზუალურ ელემენტი.
+    internal class Student
+    {
+        private string gvari, saxeli;
+        private int asaki;
+        public void Minicheba(string par_1, string par_2, int par_3)
+        {
+            gvari = par_1;
+            saxeli = par_2;
+            asaki = par_3;
+        }
+
+        public void Gamotana(Label lab_1)
+        {
+            lab_1.Text = "გვარი: " + gvari +
+                         "\nსახელი: " + saxeli +
+                         "\nასაკი: " + asaki.ToString();
+        }
+    }
+}
