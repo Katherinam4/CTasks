4091fb13433ef0ff0229e47ca93373f925efbb48
Katherinam4 <84234484+Katherinam4@users.noreply.github.com>
2024-04-22 21:27:38 -0700

    Add files via upload

---
+++ Praqtikuli_Davaleba_2/Praqtikuli_Davaleba_2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Praqtikuli_Davaleba_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int fudze, simagle;
+            int fart_mart;
+
+            fudze = int.Parse(textBox1.Text);
+            simagle = int.Parse(textBox2.Text);
+
+
+            Martkutxedi obj_mart = new Martkutxedi(fudze, simagle);
+            fart_mart = obj_mart.Fart_Mart();
+
+            label4.Text = fart_mart.ToString();
+
+        }
+    }
+}
---
+++ Praqtikuli_Davaleba_2/Praqtikuli_Davaleba_2/Sabazo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praqtikuli_Davaleba_2
+{
+    //    შექმენით საბაზო კლასი, რომელიც შეიცავს დაცულ ცვლადს: მართკუთხედის
+    //ფუძეს; ღია კონსტრუქტორს, რომელიც დაცულ ცვლადს მნიშვნელობას ანიჭებს.
+    //შექმენით მართკუთხედის მემკვიდრე კლასი, რომელიც დამატებით შეიცავს ღია
+    //ცვლადს - მართკუთხედის სიმაღლეს და პრივატულ ცვლადს - მართკუთხედი
+    //ფართობს; ღია მეთოდს, რომელიც გამოთვლის და აბრუნებს მართკუთხედის
+    //ფართობის მნიშვნელობას.მთავარ პროგრამაში შექმენით მემკვიდრე კლასის
+    //ტიპის მქონე ობიექტი და გამოიძახეთ მეთოდი.
+    internal class Sabazo
+    {
+        protected int fudze;
+
+        public Sabazo(int fudze)
+        {
+            this.fudze = fudze;
+        }
+    }
+
+
+    class Martkutxedi : Sabazo
+    {
+        public int simagle;
+        private int fartobi;
+
+        public Martkutxedi(int fudze, int simagle) : base(fudze)
+        {
+            this.simagle = simagle;
+        }
+
+        public int Fart_Mart()
+        {
+            fartobi = fudze * simagle;
+            return fartobi;
+        }
+
+    }
+}
